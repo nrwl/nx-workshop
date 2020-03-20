2b8cce48f1b287371a3e8a5d5b6104e033de4d0b
Rares Matei <matei.rar@gmail.com>
2020-03-20 14:38:36 +0000

    finish lab 10

--- libs/store/feature-game-detail/src/lib/game-detail/game-detail.component.ts
+++ libs/store/feature-game-detail/src/lib/game-detail/game-detail.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 import { map, switchMap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { formatRating } from '@bg-hoard/store/util-formatters';
+import { Game } from '@bg-hoard/util-interface';
 
 @Component({
   selector: 'bg-hoard-game-detail',
@@ -14,7 +15,7 @@ export class GameDetailComponent {
 
   game$ = this.route.paramMap.pipe(
     map((params: ParamMap) => params.get('id')),
-    switchMap(id => this.http.get<any>(`/api/games/${id}`))
+    switchMap(id => this.http.get<Game>(`/api/games/${id}`))
   );
   formatRating = formatRating;
 }
