d5734ca98d94b78a9e390546f1455877f111095e
Rares Matei <matei.rar@gmail.com>
2020-11-17 00:29:50 +0000

    finish lab 6

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
     '<rootDir>/apps/store',
     '<rootDir>/libs/store/ui-shared',
     '<rootDir>/libs/store/util-formatters',
+    '<rootDir>/libs/store/feature-game-detail',
   ],
 };
