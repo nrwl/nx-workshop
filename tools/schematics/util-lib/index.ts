cf9bb5ea8604e605030f8841326823ca93d20635
Rares Matei <matei.rar@gmail.com>
2020-11-17 11:44:26 +0000

    add schematic changes

--- tools/schematics/util-lib/index.ts
+++ tools/schematics/util-lib/index.ts
@@ -3,7 +3,10 @@ import { chain, externalSchematic, Rule } from '@angular-devkit/schematics';
 export default function (schema: any): Rule {
   return chain([
     externalSchematic('@nrwl/workspace', 'lib', {
-      name: schema.name,
+      name: `util-${schema.name}`,
+      linter: 'tslint',
+      directory: schema.directory,
+      tags: `type:util', scope:${schema.directory}`,
     }),
   ]);
 }
