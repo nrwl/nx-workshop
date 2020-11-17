7cd7d9dd429476e2caad72c0927b7e6c36c41f46
Rares Matei <matei.rar@gmail.com>
2020-11-17 12:29:00 +0000

    finish lab 14

--- tools/schematics/sort-project-references/index.ts
+++ tools/schematics/sort-project-references/index.ts
@@ -1,13 +1,27 @@
 import { chain, Rule } from '@angular-devkit/schematics';
-import { updateJsonInTree } from '@nrwl/workspace';
+import { updateJsonInTree, formatFiles } from '@nrwl/workspace';
 
-function incrementVersion(): Rule {
-  return updateJsonInTree('workspace.json', (json) => {
-    json.version++;
+function sortKeys(file: string): Rule {
+  return updateJsonInTree(file, (json) => {
+    json.projects = sortObjectKeys(json.projects);
     return json;
   });
 }
 
+function sortObjectKeys(obj: any) {
+  const sorted = {};
+  Object.keys(obj)
+    .sort()
+    .forEach((key) => {
+      sorted[key] = obj[key];
+    });
+  return sorted;
+}
+
 export default function (): Rule {
-  return chain([incrementVersion()]);
+  return chain([
+    sortKeys('workspace.json'),
+    sortKeys('nx.json'),
+    formatFiles(),
+  ]);
 }
