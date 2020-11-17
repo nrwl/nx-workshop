61062bd8f3dc36676b0a8b30edf5d216b1c77ec5
Rares Matei <matei.rar@gmail.com>
2020-11-17 00:44:52 +0000

    add rest of lab 10 files

---
+++ apps/store-ui-shared-e2e/src/integration/header/header.component.spec.ts
@@ -0,0 +1,9 @@
+describe('store-ui-shared', () => {
+  beforeEach(() =>
+    cy.visit('/iframe.html?id=headercomponent--primary&knob-title=')
+  );
+
+  it('should render the component', () => {
+    cy.get('bg-hoard-header').should('exist');
+  });
+});
