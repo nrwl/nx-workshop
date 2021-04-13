a29d49e51ee2482d1347d88933701136850add22
Rares Matei <matei.rar@gmail.com>
2021-04-13 23:13:00 +0100

    lab 13

--- tools/generators/util-lib/index.ts
+++ tools/generators/util-lib/index.ts
@@ -2,7 +2,11 @@ import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
 export default async function (host: Tree, schema: any) {
-  await libraryGenerator(host, { name: schema.name });
+  await libraryGenerator(host, {
+    name: `util-${schema.name}`,
+    directory: schema.directory,
+    tags: `type:util, scope:${schema.directory}`,
+  });
   await formatFiles(host);
   return () => {
     installPackagesTask(host);
