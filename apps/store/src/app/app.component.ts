089257deb6de56cfd87ee32c34ca5d3c2d374457
Rares Matei <matei.rar@gmail.com>
2021-04-13 22:28:28 +0100

    finish lab 8

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -1,15 +1,16 @@
-
 import { Component } from '@angular/core';
-import { getAllGames } from '../fake-api';
 import { formatRating } from '@bg-hoard/store/util-formatters';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'bg-hoard-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  constructor(private http: HttpClient) {}
+
   title = 'Board Game Hoard';
   formatRating = formatRating;
-  games = getAllGames();
+  games = this.http.get<any[]>('/api/games');
 }
--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     MatCardModule,
+    HttpClientModule,
     StoreUiSharedModule,
     RouterModule.forRoot(
       [
