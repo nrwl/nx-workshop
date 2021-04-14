6bf7420aad008c7ce32f25ba2bbd9e8dd0b8f5f5
Rares Matei <matei.rar@gmail.com>
2021-04-14 15:17:05 +0100

    add cherry picking script

---
+++ tools/cherry-pick-all.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+hash=$1
+
+git checkout starting-lab2
+git cherry-pick "$hash"
+git checkout starting-lab3
+git cherry-pick "$hash"
+git checkout starting-lab3
+git cherry-pick "$hash"
+git checkout starting-lab4
+git cherry-pick "$hash"
+git checkout starting-lab5
+git cherry-pick "$hash"
+git checkout starting-lab6
+git cherry-pick "$hash"
+git checkout starting-lab7
+git cherry-pick "$hash"
+git checkout starting-lab8
+git cherry-pick "$hash"
+git checkout starting-lab9
+git cherry-pick "$hash"
+git checkout starting-lab10
+git cherry-pick "$hash"
+git checkout starting-lab11
+git cherry-pick "$hash"
+git checkout starting-lab12
+git cherry-pick "$hash"
+git checkout starting-lab13
+git cherry-pick "$hash"
+git checkout starting-lab14
+git cherry-pick "$hash"
+git checkout starting-lab15
+git cherry-pick "$hash"
+git checkout starting-lab16
+git cherry-pick "$hash"
+git checkout starting-lab17
+git cherry-pick "$hash"
+git checkout starting-lab18
+git cherry-pick "$hash"
+git checkout starting-lab19
+git cherry-pick "$hash"
+git checkout starting-lab20-frontend
+git cherry-pick "$hash"
+git checkout starting-lab21-frontend
+git cherry-pick "$hash"
+git checkout starting-lab20-api
+git cherry-pick "$hash"
+git checkout starting-lab21-api
+git cherry-pick "$hash"
+git checkout starting-lab22
+git cherry-pick "$hash"
+git checkout final-solution
+git cherry-pick "$hash"
