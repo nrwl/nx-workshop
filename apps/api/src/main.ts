bc4c32973423102b3ff15ca5150d1231cdd89fcb
Rares Matei <matei.rar@gmail.com>
2021-04-14 14:42:19 +0100

    finish lab 19

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.enableCors();
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
