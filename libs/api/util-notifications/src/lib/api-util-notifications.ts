75c4cc4a19b4fa473195a90a19426bd3bc935604
Rares Matei <matei.rar@gmail.com>
2021-05-28 12:54:51 +0100

    update yarn lock

--- libs/api/util-notifications/src/lib/api-util-notifications.ts
+++ libs/api/util-notifications/src/lib/api-util-notifications.ts
@@ -1,5 +1,3 @@
-import { StoreFeatureGameDetailModule } from '@bg-hoard/store/feature-game-detail';
-
 export function sendNotification(clientId: string) {
   console.log('sending notification to client: ', clientId);
 }
