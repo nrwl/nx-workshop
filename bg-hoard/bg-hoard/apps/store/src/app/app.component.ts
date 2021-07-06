1bd1c34d8d6b80635042da389bfe48820edd9514
Matthew Thor <matthew.thor@screencastify.com>
2021-07-06 12:35:13 -0500

    end of lab 5

--- bg-hoard/bg-hoard/apps/store/src/app/app.component.ts
+++ bg-hoard/bg-hoard/apps/store/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { getAllGames } from '../fake-api';
+import { formatRating } from '@bg-hoard/store/util-formatters';
 
 @Component({
   selector: 'bg-hoard-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  formatRating = formatRating;
   title = 'Board Game Hoard';
   games = getAllGames();
 }
