e447c43a0a3e67a57310d0d618c444a72d2680f2
Matthew Thor <matthew.thor@screencastify.com>
2021-07-06 13:12:22 -0500

    end of lab 6

--- bg-hoard/bg-hoard/apps/store/src/app/app.module.ts
+++ bg-hoard/bg-hoard/apps/store/src/app/app.module.ts
@@ -10,7 +10,18 @@ import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    RouterModule.forRoot(
+      [
+        {
+          path: 'game/:id',
+          loadChildren: () =>
+            import('@bg-hoard/store/feature-game-detail').then(
+              (module) => module.StoreFeatureGameDetailModule
+            ),
+        },
+      ],
+      { initialNavigation: 'enabledBlocking' }
+    ),
     MatCardModule,
     StoreUiSharedModule,
   ],
