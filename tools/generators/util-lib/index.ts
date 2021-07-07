e3af87221b83c0bde6882a7f6c00961f910bc709
Martin Gutfeldt <martin@screencastify.com>
2021-07-07 12:41:50 -0500

    Run generator (fix scope)

--- tools/generators/util-lib/index.ts
+++ tools/generators/util-lib/index.ts
@@ -3,7 +3,7 @@ import { libraryGenerator } from '@nrwl/workspace/generators';
 
 interface LibSchema {
   name: string;
-  directory: 'store' | 'api' | 'shared'
+  directory: 'api' | 'store' | 'shared';
 }
 
 export default async function (host: Tree, schema: LibSchema) {
