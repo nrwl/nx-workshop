3c15d15720db676975393211eca6b9e5bb4d950f
Martin Gutfeldt <martin@screencastify.com>
2021-07-07 12:09:03 -0500

    Trying out update scope generator

---
+++ tools/generators/update-scope-schema/index.ts
@@ -0,0 +1,10 @@
+import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
+import { libraryGenerator } from '@nrwl/workspace/generators';
+import { updateJson } from '@nrwl/devkit';
+
+export default async function (host: Tree, schema: any) {
+  updateJson(host, 'workspace.json', (json) => {
+    json.defaultProject = 'api';
+    return json;
+  })
+}
