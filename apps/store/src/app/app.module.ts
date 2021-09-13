ba3ac02d380c5ec9834d13f489da25308c8a08f1
Rares Matei <matei.rar@gmail.com>
2021-09-13 12:15:41 +0100

    finish lab 8

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
     StoreUiSharedModule,
     MatCardModule,
     RouterModule.forRoot(
