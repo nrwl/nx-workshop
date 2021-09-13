18b7eb93b9a302377063a3b740660b6334865097
Rares Matei <matei.rar@gmail.com>
2021-09-13 12:15:46 +0100

    finish lab 20

--- libs/store/feature-game-detail/src/lib/game-detail/game-detail.component.ts
+++ libs/store/feature-game-detail/src/lib/game-detail/game-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { map, switchMap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
@@ -11,11 +11,15 @@ import { Game } from '@bg-hoard/util-interface';
   styleUrls: ['./game-detail.component.scss'],
 })
 export class GameDetailComponent {
-  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    @Inject('baseUrl') private baseUrl: string
+  ) {}
 
   game$ = this.route.paramMap.pipe(
     map((params: ParamMap) => params.get('id')),
-    switchMap((id) => this.http.get<Game>(`/api/games/${id}`))
+    switchMap((id) => this.http.get<Game>(`${this.baseUrl}/api/games/${id}`))
   );
   formatRating = formatRating;
 }
