b0830c8810b62583394698c86992c1a27981e706
Rares Matei <matei.rar@gmail.com>
2021-09-13 12:15:40 +0100

    finish lab 9

---
+++ libs/util-interface/src/lib/api-util-interface.spec.ts
@@ -0,0 +1,7 @@
+import { apiUtilInterface } from './api-util-interface';
+
+describe('apiUtilInterface', () => {
+  it('should work', () => {
+    expect(apiUtilInterface()).toEqual('api-util-interface');
+  });
+});
