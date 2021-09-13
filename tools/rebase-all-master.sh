61fe2e946c77bec9b7007bf9c9ab9cad71e5a127
Rares Matei <matei.rar@gmail.com>
2021-09-13 11:54:56 +0100

    rebase all master script

---
+++ tools/rebase-all-master.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+git checkout starting-lab2
+git rebase master --ignore-date
+git checkout starting-lab3
+git rebase master --ignore-date
+git checkout starting-lab4
+git rebase master --ignore-date
+git checkout starting-lab5
+git rebase master --ignore-date
+git checkout starting-lab6
+git rebase master --ignore-date
+git checkout starting-lab7
+git rebase master --ignore-date
+git checkout starting-lab8
+git rebase master --ignore-date
+git checkout starting-lab9
+git rebase master --ignore-date
+git checkout starting-lab10
+git rebase master --ignore-date
+git checkout starting-lab11
+git rebase master --ignore-date
+git checkout starting-lab12
+git rebase master --ignore-date
+git checkout starting-lab13
+git rebase master --ignore-date
+git checkout starting-lab14
+git rebase master --ignore-date
+git checkout starting-lab15
+git rebase master --ignore-date
+git checkout starting-lab16
+git rebase master --ignore-date
+git checkout starting-lab17
+git rebase master --ignore-date
+git checkout starting-lab18
+git rebase master --ignore-date
+git checkout starting-lab19
+git rebase master --ignore-date
+git checkout starting-lab20-option1
+git rebase master --ignore-date
+git checkout starting-lab21-option1
+git rebase master --ignore-date
+git checkout starting-lab20-option2
+git rebase master --ignore-date
+git checkout starting-lab21-option2
+git rebase master --ignore-date
+git checkout starting-lab22
+git rebase master --ignore-date
+git checkout final-solution
+git rebase master --ignore-date
+git checkout master
