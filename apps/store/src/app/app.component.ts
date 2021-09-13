f45018c8b85f8a0cec4d65aba8267436dd36a589
Rares Matei <matei.rar@gmail.com>
2021-09-13 12:15:43 +0100

    finish lab 2

---
+++ apps/store/src/app/app.component.ts
@@ -0,0 +1,13 @@
+
+import { Component } from '@angular/core';
+import { getAllGames } from '../fake-api';
+
+@Component({
+  selector: 'bg-hoard-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Board Game Hoard';
+  games = getAllGames();
+}
