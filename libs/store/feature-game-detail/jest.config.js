0a7557f594ecb9a9db183a9ada4916063593f735
Rares Matei <matei.rar@gmail.com>
2021-09-13 12:15:43 +0100

    finish lab 6

---
+++ libs/store/feature-game-detail/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  displayName: 'store-feature-game-detail',
+  preset: '../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../../coverage/libs/store/feature-game-detail',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+  },
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
