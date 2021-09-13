46aec5323910d1667ed4342eabe2a4035d6651a7
Rares Matei <matei.rar@gmail.com>
2021-09-13 12:15:40 +0100

    finish lab 11

--- apps/store-ui-shared-e2e/src/integration/header/header.component.spec.ts
+++ apps/store-ui-shared-e2e/src/integration/header/header.component.spec.ts
@@ -1,6 +1,10 @@
 describe('store-ui-shared', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=headercomponent--primary&args=title;'));
-  it('should render the component', () => {
-    cy.get('bg-hoard-header').should('exist');
+  beforeEach(() =>
+    cy.visit(
+      '/iframe.html?id=headercomponent--primary&args=title:BoardGameHoard;'
+    )
+  );
+  it('should show the title', () => {
+    cy.get('bg-hoard-header').contains('BoardGameHoard');
   });
 });
