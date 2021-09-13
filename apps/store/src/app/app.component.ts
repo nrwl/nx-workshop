eac7045baf2ed7ed50a05b705293e2d59c0ce987
Rares Matei <matei.rar@gmail.com>
2021-09-13 12:15:40 +0100

    finish lab 8

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { getAllGames } from '../fake-api';
 import { formatRating } from '@bg-hoard/store/util-formatters';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'bg-hoard-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(private http: HttpClient) {}
+
   title = 'Board Game Hoard';
-  games = getAllGames();
   formatRating = formatRating;
+  games = this.http.get<any[]>('/api/games');
 }
