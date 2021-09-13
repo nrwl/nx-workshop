4a182194c29c484e646a05a77179027f9fae0ded
Rares Matei <matei.rar@gmail.com>
2021-09-13 12:15:40 +0100

    finish lab 8

--- libs/store/feature-game-detail/src/lib/store-feature-game-detail.module.ts
+++ libs/store/feature-game-detail/src/lib/store-feature-game-detail.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { GameDetailComponent } from './game-detail/game-detail.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   imports: [
     CommonModule,
     MatCardModule,
+    MatButtonModule,
     RouterModule.forChild([
       { path: '', pathMatch: 'full', component: GameDetailComponent },
     ]),
