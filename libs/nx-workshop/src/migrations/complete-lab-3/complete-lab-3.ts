9a60452268d609b15d809d7388a11c6187af128a
Caleb Ukle <caleb.ukle+github@pm.me>
2022-04-11 15:50:33 -0500

    fix(lab 3): prevent duplicating the styles if run multiple times (#69)

--- libs/nx-workshop/src/migrations/complete-lab-3/complete-lab-3.ts
+++ libs/nx-workshop/src/migrations/complete-lab-3/complete-lab-3.ts
@@ -1,14 +1,17 @@
 import {
   formatFiles,
+  readProjectConfiguration,
   Tree,
   updateProjectConfiguration,
-  readProjectConfiguration,
 } from '@nrwl/devkit';
 
 export default async function update(host: Tree) {
   const config = readProjectConfiguration(host, 'store');
-  config.targets['build'].options.styles.push(
-    `./node_modules/@angular/material/prebuilt-themes/deeppurple-amber.css`
+  config.targets['build'].options.styles = Array.from(
+    new Set([
+      ...config.targets['build'].options.styles,
+      `./node_modules/@angular/material/prebuilt-themes/deeppurple-amber.css`,
+    ])
   );
   updateProjectConfiguration(host, 'store', config);
   await formatFiles(host);
