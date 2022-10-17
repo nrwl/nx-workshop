55a114f0813f2a60cced8f58b24803889c52de2d
Miroslav Jonas <missing.manual@gmail.com>
2022-10-17 14:28:05 -0700

    fix lab 14

--- libs/nx-workshop/src/migrations/complete-lab-14/complete-lab-14.ts
+++ libs/nx-workshop/src/migrations/complete-lab-14/complete-lab-14.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { formatFiles, Tree } from '@nrwl/devkit';
-import generatorGenerator from '@nrwl/nx-plugin/src/generators/generator/generator';
+import { generatorGenerator } from '@nrwl/nx-plugin/generators';
 
 export default async function update(host: Tree) {
   await generatorGenerator(host, {
@@ -11,62 +11,74 @@ export default async function update(host: Tree) {
 
   host.write(
     'libs/internal-plugin/src/generators/update-scope-schema/generator.ts',
-    `
-    import {
-      Tree,
-      updateJson,
-      formatFiles,
-      ProjectConfiguration,
-      getProjects,
-    } from '@nrwl/devkit';
-    
-    export default async function (tree: Tree) {
-      const scopes = getScopes(getProjects(tree));
-      updateSchemaJson(tree, scopes);
-      updateSchemaInterface(tree, scopes);
-      await formatFiles(tree);
+    `import {
+  Tree,
+  updateJson,
+  formatFiles,
+  ProjectConfiguration,
+  getProjects,
+} from '@nrwl/devkit';
+
+export default async function (tree: Tree) {
+  addScopeIfMissing(tree);
+  const scopes = getScopes(getProjects(tree));
+  updateSchemaJson(tree, scopes);
+  updateSchemaInterface(tree, scopes);
+  await formatFiles(tree);
+}
+
+function addScopeIfMissing(tree: Tree) {
+  const projectMap = getProjects(tree);
+  Array.from(projectMap.keys()).forEach((projectName) => {
+    const project = projectMap.get(projectName);
+    if (!project.tags.some((tag) => tag.startsWith('scope:'))) {
+      const scope = projectName.split('-')[0];
+      project.tags.push(\`scope:\${scope}\`);
+      updateProjectConfiguration(tree, projectName, project);
     }
-    
-    function getScopes(projectMap: Map<string, ProjectConfiguration>) {
-      const projects: any[] = Array.from(projectMap.values());
-      const allScopes: string[] = projects
-        .map((project) =>
-          project.tags.filter((tag: string) => tag.startsWith('scope:'))
-        )
-        .reduce((acc, tags) => [...acc, ...tags], [])
-        .map((scope: string) => scope.slice(6));
-      return Array.from(new Set(allScopes));
-    }
-    
-    function updateSchemaJson(tree: Tree, scopes: string[]) {
-      updateJson(
-        tree,
-        'libs/internal-plugin/src/generators/util-lib/schema.json',
-        (schemaJson) => {
-          schemaJson.properties.directory['x-prompt'].items = scopes.map(
-            (scope) => ({
-              value: scope,
-              label: scope,
-            })
-          );
-          return schemaJson;
-        }
+  });
+}
+
+function getScopes(projectMap: Map<string, ProjectConfiguration>) {
+  const projects: any[] = Array.from(projectMap.values());
+  const allScopes: string[] = projects
+    .map((project) =>
+      project.tags.filter((tag: string) => tag.startsWith('scope:'))
+    )
+    .reduce((acc, tags) => [...acc, ...tags], [])
+    .map((scope: string) => scope.slice(6));
+  return Array.from(new Set(allScopes));
+}
+
+function updateSchemaJson(tree: Tree, scopes: string[]) {
+  updateJson(
+    tree,
+    'libs/internal-plugin/src/generators/util-lib/schema.json',
+    (schemaJson) => {
+      schemaJson.properties.directory['x-prompt'].items = scopes.map(
+        (scope) => ({
+          value: scope,
+          label: scope,
+        })
       );
+      return schemaJson;
     }
-    
-    function updateSchemaInterface(tree: Tree, scopes: string[]) {
-      const joinScopes = scopes.map((s) => \`'\${s}'\`).join(' | ');
-      const interfaceDefinitionFilePath =
-        'libs/internal-plugin/src/generators/util-lib/schema.d.ts';
-      const newContent = \`export interface UtilLibGeneratorSchema {
-      name: string;
-      directory: \${joinScopes};
-    }\`;
-      tree.write(interfaceDefinitionFilePath, newContent);
-    }
-    
+  );
+}
+
+function updateSchemaInterface(tree: Tree, scopes: string[]) {
+  const joinScopes = scopes.map((s) => \`'\${s}'\`).join(' | ');
+  const interfaceDefinitionFilePath =
+    'libs/internal-plugin/src/generators/util-lib/schema.d.ts';
+  const newContent = \`export interface UtilLibGeneratorSchema {
+  name: string;
+  directory: \${joinScopes};
+}\`;
+  tree.write(interfaceDefinitionFilePath, newContent);
+}
 `
   );
+
   host.write(
     'libs/internal-plugin/src/generators/update-scope-schema/schema.json',
     `
@@ -76,7 +88,7 @@ export default async function update(host: Tree) {
       "$id": "UpdateScopeSchema",
       "properties": {}
     }
-    
+
 `
   );
 
@@ -84,55 +96,5 @@ export default async function update(host: Tree) {
     'libs/internal-plugin/src/generators/update-scope-schema/schema.d.ts'
   );
 
-  host.write(
-    'tools/generators/util-lib/index.ts',
-    `
-    import {
-      formatFiles,
-      ProjectConfiguration,
-      Tree,
-      updateJson,
-    } from '@nrwl/devkit';
-    import { getProjects } from '@nrwl/devkit/src/generators/project-configuration';
-
-    function getScopes(projectMap: Map<string, ProjectConfiguration>) {
-      const projects: any[] = Object.values(projectMap);
-      const allScopes: string[] = projects
-        .map((project) =>
-          project.tags.filter((tag: string) => tag.startsWith('scope:'))
-        )
-        .reduce((acc, tags) => [...acc, ...tags], [])
-        .map((scope: string) => scope.slice(6));
-      return [...new Set(allScopes)];
-    }
-
-    function replaceScopes(content: string, scopes: string[]): string {
-      const joinScopes = scopes.map((s) => \`'\${s}'\`).join(' | ');
-      const PATTERN = /interface Schema \\{\\n.*\\n.*\\n\\}/gm;
-      return content.replace(
-        PATTERN,
-        \`interface Schema {
-          name: string;
-          directory: \${joinScopes};
-        }\`
-      );
-    }
-
-    export default async function (host: Tree) {
-      const scopes = getScopes(getProjects(host));
-      updateJson(host, 'tools/generators/util-lib/schema.json', (schemaJson) => {
-        schemaJson.properties.directory['x-prompt'].items = scopes.map((scope) => ({
-          value: scope,
-          label: scope,
-        }));
-        return schemaJson;
-      });
-      const content = host.read('tools/generators/util-lib/index.ts', 'utf-8');
-      const newContent = replaceScopes(content, scopes);
-      host.write('tools/generators/util-lib/index.ts', newContent);
-      await formatFiles(host);
-    }
-`
-  );
   await formatFiles(host);
 }
